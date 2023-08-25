6a0be31eb9cc22337c32bc70ede0c9468d4f6a10
ifebrand6 <ifebrand6@gmail.com>
2023-08-25 15:08:26 +0100

    Google SSO

--- app/auth/sign-in/sign-in-form.tsx
+++ app/auth/sign-in/sign-in-form.tsx
@@ -2,23 +2,27 @@
 
 import React from 'react'
 import Logo from '@/app/logo'
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import AppleLogo from '@/public/svgs/apple-logo.svg'
 import GoogleLogo from '@/public/svgs/google-logo.svg'
 
 const SignInForm = () => {
-  return (
-    <div className="w-full flex flex-col items-center gap-[4rem] p-4">
+ const { data: session } = useSession();
+  console.log(session)
+ if (!(session && session.user)) {
+    return (
+       <div className="w-full flex flex-col items-center gap-[4rem] p-4">
       <Logo />
 
       <div className="text-white flex flex-col items-center w-full justify-center bg-[rgba(28,27,27,0.90)] backdrop-blur-[0.375rem] gap-6 lg:p-[4rem] p-3 pt-8 rounded-[0.375rem] md:max-w-[327px] lg:max-w-[504px]">
         <h4 className="text-h-4 z-10 ">Sign In</h4>
 
-        <button className="flex w-full rounded-lg bg-black-main h-[3rem] justify-between items-center px-6">
+        <button className="flex w-full rounded-lg bg-black-main h-[3rem] justify-between items-center px-6" onClick={() => signIn()} >
           Sign In with Google <GoogleLogo />
         </button>
 
-        <button className="flex w-full rounded-lg bg-black-main h-[3rem] justify-between items-center px-6">
+        <button className="flex w-full rounded-lg bg-black-main h-[3rem] justify-between items-center px-6 hidden">
           Sign In with Apple <AppleLogo />
         </button>
 
@@ -31,7 +35,13 @@ const SignInForm = () => {
         </div>
       </div>
     </div>
-  )
+    );
+  }
+  return (
+    <button onClick={() => signIn()} className="text-green-600 ml-auto">
+     Redirect to home
+    </button>
+  );
 }
 
 export default SignInForm
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from "./components/Providers";
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} bg-black-dark`}>{children}</body>
+      <body className={`${poppins.className} bg-black-dark`}>
+      <Providers>
+         {children}
+      </Providers>
+    </body>
     </html>
   )
 }
